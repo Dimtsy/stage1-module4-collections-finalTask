5cb25f7e02f7512a9007589742bec56d2af65205
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-04-27 19:05:10 +0300

    Correction Stage 1 Final Tasks

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -7,11 +7,11 @@ public class MapFromKeysCreator {
         Map<Integer, Set<String>> map = new HashMap<>();
         for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
             if (map.containsKey(entry.getKey().length())) {
-                Set set = map.get(entry.getKey().length());
+                Set<String> set = map.get(entry.getKey().length());
                 set.add(entry.getKey());
                 map.put(entry.getKey().length(), set);
             } else {
-                Set setFirst = new HashSet();
+                Set<String> setFirst = new HashSet<>();
                 setFirst.add(entry.getKey());
                 map.put(entry.getKey().length(), setFirst);
             }
